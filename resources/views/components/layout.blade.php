ba72aa3f9bb3e1a8b7297595c60509d7264c369f
desteban <esteban28ec@outlook.com>
2024-07-17 00:17:52 -0500

    utilizar taildwind

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @isset($title)
+        <title>Tareapp - {{ $title }}</title>
+    @else
+        <title>Tareapp</title>
+    @endisset
+
+    <meta name="description" content="{{ $metaDescription ?? 'Tareapp es una aplicacion que te permite llevar el control de tu dia a dia.'}}"/>
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite('resources/css/app.css')
+</head>
+
+<body>
+    <header class="px-4 py-2 border-b" >
+        @auth
+            <a href="/">Tareas</a>
+        @else
+            <div class="flex space-x-2 items-center">
+                <a href="/">Login</a>
+                <a href="/">Registro</a>
+            </div>
+        @endauth
+    </header>
+
+    <main>
+        {{ $slot }}
+    </main>
+
+    <footer>
+
+    </footer>
+</body>
+
+</html>
